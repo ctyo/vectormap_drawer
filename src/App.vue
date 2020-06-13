0385c515a8768b414a50a50f0c436af4c087d124
ctyo <ctyo@live.jp>
2020-06-14 08:54:29 +0900

    add fork me line

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div id="app">
+    <a
+      class="forkme-line"
+      href="https://github.com/ctyo/vectormap_drawer"
+    ><img
+        width="149"
+        height="149"
+        src="https://github.blog/wp-content/uploads/2008/12/forkme_left_darkblue_121621.png?resize=149%2C149"
+        class="attachment-full size-full"
+        alt="Fork me on GitHub"
+        data-recalc-dims="1"
+      ></a>
     <router-view />
   </div>
 </template>
@@ -20,5 +31,11 @@ html,
 body,
 #app {
   height: 100%;
+}
+.forkme-line {
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 99;
 }
 </style>
