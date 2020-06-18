f412e10209e5845f9cee03252fe92716f68b9345
ctyo <ctyo@live.jp>
2020-06-18 21:49:46 +0900

    fix release path

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vectormap_drawer/dist/',
+    assetsPublicPath: '/vectormap_drawer/',
 
     /**
      * Source Maps
